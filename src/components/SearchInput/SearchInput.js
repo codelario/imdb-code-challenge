e24aca2130010a7aabd256db5bf2c37f7292cd90
codelario <cabrera.dario@gmail.com>
2021-01-15 19:25:06 -0300

    WIP: Styling / Applying styles to SearchInput

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,18 +1,53 @@
 import React, { Fragment, useState } from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+
 const SearchInput = (props) => {
 
-    const [inputValue, setInputValue] = useState(null);
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            padding: '2px 4px',
+            display: 'inline-flex',
+            alignItems: 'center',
+            width: 400,
+        },
+        input: {
+            marginLeft: theme.spacing(1),
+            flex: 1,
+        },
+        iconButton: {
+            padding: 10,
+        },
+    }));
 
-    const handleQueryButton = () => props.searchQueryFn(inputValue);
-
-    const updateInputValue = (evt) => setInputValue(evt.target.value);
+    const classes = useStyles();
+    const handleQueryButton = (evt) => {
+        if (evt.target.value.length < 3) {
+            return;
+        }
+        props.searchQueryFn(evt.target.value);
+    };
 
     return (
         <Fragment>
-            <label for="site-search">Search your favorite movie:</label>
-            <input type="search" id="site-search" aria-label="Search your favourite movie" onChange={updateInputValue} />
-            <button onClick={handleQueryButton}>Search</button>
+            <Paper component="form" className={classes.root}>
+                <IconButton className={classes.iconButton} aria-label="menu">
+                </IconButton>
+                <InputBase
+                    className={classes.input}
+                    placeholder="Search IMDB Movies"
+                    inputProps={{ 'aria-label': 'search movies' }}
+                    onChange={handleQueryButton}
+                />
+                <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
         </Fragment >
     );
 }
