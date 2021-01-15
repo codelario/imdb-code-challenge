3e7af6fd1c7b18e580359f975b299d9a98bae62d
codelario <cabrera.dario@gmail.com>
2021-01-15 16:30:30 -0300

    WIP: Adding Modal Component

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -1,14 +1,21 @@
 import React, { Fragment, useEffect, useState } from 'react';
 
 import Rating from "../Rating/Rating";
+import Modal from '@material-ui/core/Modal';
+
 const Discover = (props) => {
 
     const [items, setItems] = useState([]);
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         const setListItems = (props) => setItems(props.data);
         setListItems(props);
     });
+
+    const handleOpen = () => {
+        setShowModal(!showModal);
+    }
 
     return (
         <Fragment>
@@ -23,9 +30,21 @@ const Discover = (props) => {
                             value={Math.ceil(item.vote_average / 2)}
                             edit={false}
                         ></Rating>
+                        <button type="button" onClick={handleOpen}>
+                            Open Modal
+                        </button>
                     </li>
                 ))}
             </ul>
+
+            <Modal
+                open={showModal}
+                onClose={handleOpen}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                <div>Modal Content</div>
+            </Modal>
         </Fragment >
     );
 }
--- src/components/Rating/Rating.stories.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-//TODO: Remove Stories
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Rating from './Rating';
-
-storiesOf('Rating', module).add('default', () => <Rating />);
