74f1538e8367f53016785542856e9b9ad4598f87
codelario <cabrera.dario@gmail.com>
2021-01-15 18:59:34 -0300

    WIP: Styling / Applying new styles to movies list

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -1,14 +1,40 @@
 import React, { Fragment, useEffect, useState } from 'react';
 
-import Rating from "../Rating/Rating";
-import DetailView from "../DetailView/DetailView";
+import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+
+import configs from "../../App.config";
+import DetailView from "../DetailView/DetailView";
 
 const Discover = (props) => {
 
     const [items, setItems] = useState([]);
     const [showModal, setShowModal] = useState(false);
     const [selectedItem, setSelectedItem] = useState(false);
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-around',
+            overflow: 'hidden',
+            backgroundColor: theme.palette.background.paper,
+        },
+        gridList: {
+            width: 1200,
+            height: 'auto',
+        },
+        icon: {
+            color: 'rgba(255, 255, 255, 0.54)',
+        },
+    }));
+
+    const classes = useStyles();
 
     useEffect(() => {
         const setListItems = (props) => setItems(props.data);
@@ -24,19 +50,26 @@ const Discover = (props) => {
         <Fragment>
             <div>Movies List</div>
             <p>{props.isFetching ? 'Loading Movies...' : ''}</p>
-            <ul>
-                {items && items.map(item => (
-                    <li key={item.id} onClick={() => handleOpenModal(item)}>
-                        {item.original_title}&nbsp;
-                        {item.vote_average}
-                        <Rating
-                            value={Math.ceil(item.vote_average / 2)}
-                            edit={false}
-                        ></Rating>
-                    </li>
-                ))}
-            </ul>
-
+            <div className={classes.root}>
+                <GridList cellHeight={160} cols={5} className={classes.gridList}>
+                    <GridListTile key="Subheader" cols={5} style={{ height: 'auto' }}>
+                        <ListSubheader component="div">December</ListSubheader>
+                    </GridListTile>
+                    {items && items.map((tile) => (
+                        <GridListTile key={tile.id} cols={1} onClick={() => handleOpenModal(tile)}>
+                            <img src={`${configs.IMAGE_URL}${tile.poster_path}`} alt={tile.title} />
+                            <GridListTileBar
+                                title={tile.title}
+                                subtitle={<span>Release Date: {tile.release_date}</span>}
+                                actionIcon={
+                                    <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
+                                    </IconButton>
+                                }
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+            </div>
             <Modal
                 open={showModal}
                 onClose={handleOpenModal}
